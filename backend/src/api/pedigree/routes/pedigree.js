78bd4126f3c365c9671b8b97741d3209acaf0a90
Larry Volz <imaginologist@gmail.com>
2022-09-26 12:14:36 -0400

    removed backend for hopeful Heroku deployment

--- backend/src/api/pedigree/routes/pedigree.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * pedigree router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::pedigree.pedigree');
--- backend/src/api/pedigree/services/pedigree.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * pedigree service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::pedigree.pedigree');
